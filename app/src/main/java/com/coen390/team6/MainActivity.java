88b6ba9f7adf447a5afe4d1cd7a4241c3f535d6c
Elizabeth Tremblay <liztremblay01@gmail.com>
2026-03-05 21:00:21 -0500

    Enhance MainActivity and activity_main layout with data and new UI components

--- app/src/main/java/com/coen390/team6/MainActivity.java
+++ app/src/main/java/com/coen390/team6/MainActivity.java
@@ -10,15 +10,34 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView heartrateText, fatigueText, bluetoothText, batteryText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        bindViews();
+        bindData();
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void bindViews() {
+        heartrateText = findViewById(R.id.heartrateText);
+        fatigueText = findViewById(R.id.fatigueText);
+        bluetoothText = findViewById(R.id.bluetoothText);
+        batteryText = findViewById(R.id.batteryText);
+    }
+
+    public void bindData() {
+        heartrateText.setText(toString(R.string.heartrate_value));
+        fatigueText.setText(toString(R.string.fatigue_value));
+        bluetoothText.setText(toString(R.string.bluetooth_value));
+        batteryText.setText(toString(R.string.battery_value));
     }
 }
